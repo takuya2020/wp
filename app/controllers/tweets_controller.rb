604e3a0f4ca549b753056ba40167d0e89abcb3ea
takuya <zqiaoben51@gmail.com>
2020-06-23 17:16:37 +0900

    deploy

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,9 @@
 class TweetsController < ApplicationController
+    before_action :set_tweet, only: [:show]   
+    before_action :move_to_index, except: [:index, :show]    
+
     def index
-        @tweets = Tweet.all
+        @tweets = Tweet.includes(:user)
     end
 
     def new
@@ -11,9 +14,19 @@ class TweetsController < ApplicationController
         Tweet.create(tweet_params)
     end
     
-    private
-    def tweet_params
-      params.require(:tweet).permit(:name, :image, :text)
+    def show
     end
 
+    private
+    def tweet_params
+        params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
+    end
+    
+    def set_tweet
+        @tweet = Tweet.find(params[:id])
+    end
+      
+    def move_to_index
+        redirect_to action: :index unless user_signed_in?
+    end    
 end
